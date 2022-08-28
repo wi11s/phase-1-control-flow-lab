835c8e1ee8666632fa1f55309a3ac0521dd05db9
William <wms274@nyu.edu>
2022-08-28 14:52:09 -0400

    commit tree a5755e6edc4f734876fa12d8fa7f1d433da2c822

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let greeting;
+  if (ride <= 400) {
+    greeting = 'This one is on me!'; 
+  } else if (ride>400 && ride<2000) {
+    greeting = 'That will be twenty bucks.'
+  } else if (ride>=2000 && ride<=2500) {
+    greeting = 'I will gladly take your thirty bucks.'
+  } else {
+    greeting = 'No can do.'
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return 'Thank you.'
+    default: 
+      return 'Bye.'
+  }
 }
